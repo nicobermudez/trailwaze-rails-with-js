2f804791d5c913f0981169ec1677f830249377dc
Nico Bermudez <bermnicolas@gmail.com>
2019-03-29 13:15:44 -0400

    Style new itinerary page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  provider :google, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
