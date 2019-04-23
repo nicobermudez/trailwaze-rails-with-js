4a9e1ac777bf38ab1dd5ecdd97b754e9f8e17ffb
Nico Bermudez <bermnicolas@gmail.com>
2019-04-23 14:44:00 -0400

    Clean up files and clean CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  root 'sessions#new'
   get '/login', to: 'sessions#new'
   post '/sessions/create', to: "sessions#create"
   post '/users/create', to: "users#create"
